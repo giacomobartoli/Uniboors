1e2ae85ef3301486d28a57a7429214e02ddb1472
gzano <giacomo.zanotti2@studio.unibo.it>
2017-09-26 19:15:37 +0200

    presenter for login

---
+++ app/src/main/java/Model/User.kt
@@ -0,0 +1,8 @@
+package Model
+
+import org.jetbrains.annotations.NotNull
+
+/**
+ * Created by gzano on 26/09/2017.
+ */
+data class User(@NotNull var email:String,@NotNull var password:String)
--- app/src/main/java/View/WelcomeView.kt
+++ app/src/main/java/View/WelcomeView.kt
@@ -1,7 +1,18 @@
 package View
 
+import com.example.gzano.uniboors.WelcomeActivity
+import com.google.firebase.auth.FirebaseUser
+
 /**
  * Created by gzano on 26/09/2017.
  */
 interface WelcomeView {
+
+    fun updateView(user: FirebaseUser)
+
+    fun onLoginPressed()
+
+    fun getActivityContext():WelcomeActivity
+
+
 }
