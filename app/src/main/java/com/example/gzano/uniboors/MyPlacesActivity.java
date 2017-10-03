c098e4f0fd0dd0bac191b05fbd928c510e0a81ca
gzano <giacomo.zanotti2@studio.unibo.it>
2017-10-03 10:48:34 +0200

    added a search view within the toolbar, no more inflating menus

--- app/src/main/java/com/example/gzano/uniboors/MyPlacesActivity.java
+++ app/src/main/java/com/example/gzano/uniboors/MyPlacesActivity.java
@@ -22,14 +22,7 @@ public class MyPlacesActivity extends AppCompatActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.places_menu, menu);
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
+
 
         return true;
     }
