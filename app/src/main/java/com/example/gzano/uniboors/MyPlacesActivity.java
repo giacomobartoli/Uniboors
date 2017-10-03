499fa3f4247093b681f0332f3c985bf4f9ee34b5
gzano <giacomo.zanotti2@studio.unibo.it>
2017-10-03 10:34:57 +0200

    finally created a search view in activity

--- app/src/main/java/com/example/gzano/uniboors/MyPlacesActivity.java
+++ app/src/main/java/com/example/gzano/uniboors/MyPlacesActivity.java
@@ -1,8 +1,12 @@
 package com.example.gzano.uniboors;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 
@@ -20,6 +24,12 @@ public class MyPlacesActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.places_menu, menu);
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
 
         return true;
     }
